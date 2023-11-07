f2b0ff597c114b7d256a1e34da8311e6f6a267bb
h662 <h662hong@gmail.com>
2023-11-07 21:34:09 +0900

    isLoading, Chat GPT list

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 
 const App = () => {
   const [content, setContent] = useState("");
+  const [resList, setResList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onSubmitChat = async (e) => {
     try {
@@ -10,10 +12,12 @@ const App = () => {
 
       if (!content) return;
 
+      setIsLoading(true);
+
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
         {
-          model: "gpt-4",
+          model: "gpt-3.5-turbo",
           messages: [{ role: "user", content }],
         },
         {
@@ -24,9 +28,15 @@ const App = () => {
         }
       );
 
-      console.log(response);
+      setIsLoading(false);
+
+      if (response.status !== 200) return;
+
+      setResList([...resList, response.data.choices[0].message.content]);
     } catch (error) {
       console.error(error);
+
+      setIsLoading(false);
     }
   };
 
@@ -37,18 +47,33 @@ const App = () => {
     >
       <form className="flex max-w-screen-md w-full ">
         <input
-          className="grow border-2 border-pink-200 rounded-lg focus:outline-pink-400 px-2 py-1 text-lg"
+          className={`grow border-2 border-pink-200 rounded-lg focus:outline-pink-400 px-2 py-1 text-lg ${
+            isLoading && "bg-gray-300 border-gray-500"
+          }`}
           type="text"
           value={content}
           onChange={(e) => setContent(e.target.value)}
           placeholder="무엇이든 물어보세요. Chat-GPT"
+          disabled={isLoading}
         />
         <input
-          className="ml-4 px-2 py-1 bg-pink-400 rounded-lg text-gray-50 w-28"
+          className={`ml-4 px-2 py-1 rounded-lg text-gray-50 w-28 ${
+            isLoading ? "bg-gray-400" : "bg-pink-400"
+          }`}
           type="submit"
-          value="검 색"
+          value={isLoading ? "로딩중" : "검 색"}
+          disabled={isLoading}
         />
       </form>
+      <ul className="p-20 flex flex-col gap-12">
+        {resList.map((v, i) => {
+          return (
+            <li key={i}>
+              {i + 1}. {v}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
